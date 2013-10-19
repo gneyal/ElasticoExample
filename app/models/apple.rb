7ed8b54f089eef1f46e57bc9ba7b4b7b0a754f9d
Eyal-Mac <gneyal@gmail.com>
2013-10-19 11:06:57 +0200

    sending settings manually

--- app/models/apple.rb
+++ app/models/apple.rb
@@ -23,6 +23,7 @@ class Apple < ActiveRecord::Base
 
   # include elastico only after you declared your settings and mappings json in the method prepare_elastico_settings_and_mappings_json
   include Elastico
-
   Apple.elastico_url = "http://vokdxbxi:dhkxpsqfg8fy2bnr@banyan-6062486.us-east-1.bonsai.io:9200" if Rails.env == "production"
+  send_settings_mappings_to_elasticsearch_server
+
 end
